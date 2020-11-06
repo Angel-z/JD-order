fd2648d64c33c5d5bf3b204592d058ca5e8697ae
Angel <52435162+Angel-z@users.noreply.github.com>
2020-11-06 12:46:37 +0000

    Create Refresh_Click.user.js

---
+++ Refresh_Click.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Refresh_Click
+// @namespace    1
+// @version      0.01
+// @description  JD
+// @match        https://trade.jd.com/shopping/order/getPresalInfo.action?r=商品编号
+// @author      Angel
+// ==/UserScript==
+
+(function() {
+    let time = 4000;  //延时ms
+
+    setTimeout(() => {
+        let text = document.getElementById("pre-state").innerText;  //标签中库存状态
+        
+        if (text != "无货"){
+            document.getElementById("presaleEarnest").checked=true; //勾选同意定金选项
+            document.getElementById("order-submit").click();  //点击提交订单
+        }
+        else{
+            location.reload();
+        }
+    },time);
+})();
